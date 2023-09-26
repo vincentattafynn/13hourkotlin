4898b4bb97a39c5c5d5226937b472a2d5f020ac3
vincentattafynn <vincentattafynn@gmail.com>
2023-09-26 11:47:02 +0000

    third commit gotten really far

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,25 @@
 fun main(){
-    val car1 = Classes.Car("Audi","rs7","black",4)
+    val success = Result.Success("Success")
+
+    getData(success)
 }
 
+fun getData(result: Result){
+    when (result){
+        is Result.Error -> result.showMessage()
+        is Result.Success -> result.showMessage()
+        is Result.Error.RecoverableError -> result.showMessage()
+        is Result.Error.NonRecoverableError -> result.showMessage()
+    }
+}
+sealed class Result(val message: String){
+    fun showMessage(){
+        println("Result: $message")
+    }
+    class Success(message: String): Result(message)
+    sealed class Error(message: String): Result(message){
+        class RecoverableError(exception: Exception, message: String): Error(message)
+        class NonRecoverableError(exception: Exception, message: String): Error(message)
+    }
+    class Progress(message: String): Result(message)
+}
